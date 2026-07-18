15207d757683b42126e103486e986caa0d98454a
sameera Ampili <sameeraampilisameeraampili@gmail.com>
2026-07-18 10:22:46 +0530

    palindrom.java

--- palindrom.java
+++ palindrom.java
@@ -2,20 +2,20 @@
 //leetcode-Easy
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0 || (x%10 == 0 && x!=0)){
-            return false;
-        }
-        int temp = x;
+        if(x < 0) return false;
+
+        int original = x;
         int rev = 0;
-        while(temp > 0){
-            int y = x%10;
-            rev +=rev*10 + y;
-            temp = x/10;
-        }
-        if(rev == temp){
-            return true;
-        }
-        return false;
+
+        while(x > 0){
+            int n = x%10;
+            rev = rev*10 + n;
+            x = x/10;
+        }  
+        return original == rev;
+        
+    }
+}
            
         
         
